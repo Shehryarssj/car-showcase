06ba94c92be0b12f13c8756abcac9d5b1cb0f516
Shehryar <shehryar1.ssj@gmail.com>
2023-10-06 22:07:38 +0500

    added env file

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,14 @@
+// @ts-nocheck
 import { FilterProps } from "@/Types";
 import { CarProps } from "@/Types";
+import "dotenv/config";
+
 export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, fuel, limit, model } = filters;
   const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${20}&fuel_type=${fuel}`;
   const headers = {
-    "X-RapidAPI-Key": "ff29af8622msh5a453ad6674079ap10dc7ajsnb98f5fcb3aba",
-    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.RAPIDAPIKEY,
+    "X-RapidAPI-Host": process.env.RAPIDAPIHOST,
   };
   const response = await fetch(url, { headers: headers });
   const result = await response.json();
